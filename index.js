60c00ab3bff7c061a57a2a3692922a74bdc5f0e9
Tunde Ojebisi <realtundeonline@gmail.com>
2022-01-14 21:21:33 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// function handleClicks() {
+//   let count = 0;
+//   $('.js-click-counter').text(count);
+//   $('.js-clicker').click(function (event) {
+//     count += 2;
+//     $('.js-click-counter').text(count);
+//   });
+// }
+// $(handleClicks)
+// $
